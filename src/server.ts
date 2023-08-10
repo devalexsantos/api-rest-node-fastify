d5ad7f4d8fd6daed5604f8b77930286528d5a786
Alex Santos <devalexsantos@gmail.com>
2023-08-10 09:59:51 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify'
+import { env } from './env'
+import { transactionsRoute } from './routes/transactions'
+import cookie from '@fastify/cookie'
+
+const app = fastify()
+
+app.register(cookie)
+
+app.register(transactionsRoute, {
+  prefix: 'transactions',
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
